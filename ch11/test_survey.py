b00498194a9ecf54115bb6b6ffc9226ade1f6b6f
Hikarigaoka <gh.yum.x@gmail.com>
2022-12-15 21:43:31 +0900

    오타수정

--- ch11/test_survey.py
+++ ch11/test_survey.py
@@ -5,14 +5,14 @@ class TestAnonymousSurvey(unittest.TestCase):
     """AnonymousSurvey 클래스 테스트"""
 
     def test_store_single_response(self):
-        """응답 하나가 제대레 저장되는지 테스트"""
+        """응답 하나가 제대로 저장되는지 테스트"""
         question = "What language did you first learn to speak?"
         my_survey = AnonymousSurvey(question)
         my_survey.store_response('English')
         self.assertIn('English', my_survey.responses)
 
     def test_store_three_responses(self):
-        """응답 하나가 제대레 저장되는지 테스트"""
+        """응답 세 개가 제대로 저장되는지 테스트"""
         question = "What language did you first learn to speak?"
         my_survey = AnonymousSurvey(question)
         responses = ['English', 'Spanish', 'Mandarin']
