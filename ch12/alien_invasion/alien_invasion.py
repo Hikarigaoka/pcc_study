35fd17d17e2534a35ce6a68252c9052934263735
Hikarigaoka <gh.yum.x@gmail.com>
2022-12-19 21:20:42 +0900

    12.5

--- ch12/alien_invasion/alien_invasion.py
+++ ch12/alien_invasion/alien_invasion.py
@@ -21,17 +21,23 @@ class AlienInvasion:
         """게임의 메인 루프를 시작합니다"""
 
         while True:
-            # 키보드와 마우스 이벤트를 주시합니다
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self._update_screen()
 
-            # 루프의 반복마다 화면을 다시 그립니다
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
+    def _check_events(self):
+        """키 입력과 마우스 이벤트에 반응합니다"""
+        # 키보드와 마우스 이벤트를 주시합니다
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
-            # 가장 최근에 그려진 화면을 표시합니다
-            pygame.display.flip()
+    def _update_screen(self):
+        """화면에 이미지를 업데이트하고 새 화면으로 그립니다"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        # 가장 최근에 그려진 화면을 표시합니다
+        pygame.display.flip()
 
 if __name__ == '__main__':
     # 게임 인스턴스를 만들고 게임을 실행합니다
